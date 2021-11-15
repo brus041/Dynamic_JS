c081e5289a21462f63d411aacb1fc13b71470506
Brubel <brubel@ucsd.edu>
2021-11-14 18:16:53 -0800

    some stuff done

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,65 @@
-d3.json("/samples.json").then(function(data) {
+const samples  = "https://brus041.github.io/Web_Visualization_Challenge/samples.json"
+d3.json(samples).then(function(data) {
     console.log(data);
   });
+
+// set up initial plot with 940 as id 
+function init() {
+    dat = [{
+        type: 'bar',
+        x : [20, 14, 23],
+        y : ['giraffes', 'orangutans', 'monkeys'],
+        orientation: 'h',
+        transforms: [
+            {
+              type: "sort",
+              target: "x",
+              order: "ascending"
+            }
+          ]
+    }];
+    
+    Plotly.newPlot("bar", dat);
+  }
   
+d3.json(samples).then(function(data) {
+    // create array for all ids
+    ids = []
+    for (let i = 0; i < data.samples.length; i++) {
+        ids.push(data.samples[i].id)
+    }    
+  });
+
+d3.selectAll("#selDataset").on("change", optionChanged);
+  
+function optionChanged(){
+    var dropdownMenu = d3.select("#selDataset");
+    var id = dropdownMenu.property("value");
+
+// use  id to select that portion of json data 
+// use x as sample_values, y as the names 
+d3.json(samples).then(function(data) {
+    for (let i = 0; i < data.samples.length; i++) {
+        if (id === data.samples[i].id){
+            var xs = data.samples[i].sample_values.slice(0,10);
+            var sorted = xs.sort(function sortFunction(a, b) {
+                return b - a;
+              });
+            var x = sorted.reverse();
+            var y = data.samples[i].otu_ids.slice(0,10);
+
+            Plotly.restyle("bar", "x", [x]);
+            Plotly.restyle("bar", "y", [y]);
+        }
+    }
+    // Plotly.restyle("bar", "x", [x]);
+    // Plotly.restyle("bar", "y", [y]);
+
+    
+
+  });
+}
+
+
+// display plot
+init()
