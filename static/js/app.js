eb20281b1848687e0d8e609598e6a43456bec9a4
Brubel <brubel@ucsd.edu>
2021-11-15 11:59:20 -0800

    finished

--- static/js/app.js
+++ static/js/app.js
@@ -6,8 +6,26 @@ d3.json(samples).then(function(data) {
 
 // set up initial plot with 940 as id 
 function init() {
-    // initialize initial plot manually as it will change based on user selection
-        dat = [{
+    // initialize initial plot to id 940 (the first patient) manually as it will change based on user selection
+    
+    // patient 940 demographic information
+    var dm = d3.select("#sample-metadata")
+    metad = 
+    ['ID: '+ 940,
+    'Ethnicity: '+ 'Caucasian',
+    'Age: '+ 24,
+    'Gender: ' + 'F',
+    'Location: ' + 'Beaufort/NC',
+    'BB Type: '+ 'I',
+    'Wfreq: '+ 2]
+    
+    metad.forEach((inf) => {
+        var rw = dm.append("li");
+        rw.text(inf);
+      });
+
+    // horizontal bar plot
+            dat = [{
             type: 'bar',
             x : [40, 40, 47, 50, 51, 71, 78, 113, 126, 163],
             y : ["OTU 1977", "OTU 2318", "OTU 189", "OTU 352", "OTU 1189", "OTU 41", "OTU 2264", "OTU 482", "OTU 2859", "OTU 1167"],
@@ -17,8 +35,7 @@ function init() {
         
         Plotly.newPlot("bar", dat);
         d3.json(samples).then(function(data) {
-            
-          
+    // bubble chart  
         var trace1 = {
             x: data.samples[0].otu_ids ,
             y: data.samples[0].sample_values,
@@ -36,34 +53,42 @@ function init() {
             title: 'OTU Bubble Chart',
             showlegend: false,
             height: 600,
-            width: 1500
+            width: 1300
           };
           
           Plotly.newPlot('bubble', data, layout);
         });
   }
   
+// get array of person ids and automate creation of html <option> tags with d3
 d3.json(samples).then(function(data) {
     // create array for all ids
     ids = []
     for (let i = 0; i < data.samples.length; i++) {
         ids.push(data.samples[i].id)
-    }    
+    } 
+    var menu = d3.select("#selDataset");
+    ids.forEach((i) => {
+        var row = menu.append("option");
+        row.text(i);
+      });  
   });
 
+
+// embed workflow for constructing graphs based on selection from dropdown menu
 d3.selectAll("#selDataset").on("change", optionChanged);
   
 function optionChanged(){
     var dropdownMenu = d3.select("#selDataset");
     var id = dropdownMenu.property("value");
 
-// use  id to select that portion of json data 
-// use x as sample_values, y as the names 
+// loop through data and collect data for plots based on change in drop down table value
 d3.json(samples).then(function(data) {
     for (let i = 0; i < data.samples.length; i++) {
         if (id === data.samples[i].id){
            
             // *********************display metadata for chosen id********************
+            // reset table, then append
             d3.selectAll("li").remove()
             var dem = d3.select("#sample-metadata")
             metad = 
@@ -85,8 +110,7 @@ d3.json(samples).then(function(data) {
             var sorted = xs.sort(function sortFunction(a, b) {
                 return b - a;
               });
-              
-        
+
             var x0 = sorted.reverse();
             var y0 = data.samples[i].otu_ids.slice(0,10).reverse();
             
@@ -134,6 +158,5 @@ d3.json(samples).then(function(data) {
   });
 }
 
-
-// display plots
+// display initial plots
 init()
